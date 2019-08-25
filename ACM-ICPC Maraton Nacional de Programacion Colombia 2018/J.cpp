ecaedbeb31a602799841f414f8867eb5cdbf34e9
Johnny Villegas <linkrsfan@gmail.com>
2019-08-25 00:52:00 -0500

    Add files via upload

---
+++ ACM-ICPC Maraton Nacional de Programacion Colombia 2018/J.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define fp first
+#define sp second
+#define mp make_pair
+#define ins insert
+#define uEdge(u, v) g[u].pb(v), g[v].pb(u)
+#define uwEdge(u, v, w) g[u].pb({v, w}), g[v].pb({u, w})
+#define dEdge(u, v) g[u].pb(v)
+#define dwEdge(u, v, w) g[u].pb({v, w})
+#define BOOST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define minHeap int, vector<int>, greater<int>
+#define MOD 1000000007
+
+using namespace std;
+typedef long long int lli;
+typedef pair<int, int> pii;
+lli nl, arr[0];
+vector<int> g[0];
+
+lli gcd(lli a, lli b){if(b == 0) return a;a%=b;return gcd(b, a);}
+lli lcm(lli a, lli b){return (a*b)/gcd(a, b);}
+int bs(lli sum){int l=0,r=nl-1,mid;while(l<=r){mid=(l+r)/2;if(arr[mid]==sum)return mid;if(arr[mid]<sum)l=mid+1;else r=mid-1;}return -1;}
+lli modPow(lli a, lli b){a%=MOD;lli ans=1;while(b > 0){if(b & 1) ans=ans*a%MOD;a=a*a%MOD;b>>=1;}return ans;}
+
+int main(){
+    double l, n, d, a, aux, mini;
+    lli num, dem, gcdd;
+    while(scanf("%lf %lf %lf", &l, &n, &d) != EOF){
+        if(d < 1.0) break;
+        mini=1e10;
+        for(int b = 1; b <= n; b++){
+            a=ceil((d*b)/l);
+            aux=(a*l)/b-d;
+            if(aux < mini) mini=aux, num=a*l, dem=b;
+        }
+        gcdd=gcd(num, dem);
+        num/=gcdd, dem/=gcdd;
+        printf("%lld/%lld\n", num, dem);
+    }
+    return 0;
+}
